3f5fc31fac6f95dee2ab8aa20ec47f917b048ee1
박우철 <woochul102@gmail.com>
2023-11-06 17:24:04 +0900

    customer insert 구현

--- day3/Customer01-directive/src/App.vue
+++ day3/Customer01-directive/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import CustomerVue from './components/CustomerVue.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 </script>
 
@@ -9,13 +10,12 @@ import CustomerVue from './components/CustomerVue.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <CustomerVue />
+      <HelloWorld msg="" />
     </div>
   </header>
 
   <main>
-    <TheWelcome />
-    <Test />
+    <CustomerVue />
   </main>
 </template>
 
---
+++ day3/Customer01-directive/src/components/CustomerVue.vue
@@ -0,0 +1,88 @@
+<script setup>
+import { ref } from "vue";
+const customers = ref([]);
+const num = ref("");
+const name = ref("");
+const address = ref("");
+
+
+
+let customerTmp = localStorage.getItem("customerlist");
+if (customerTmp) {
+    customers.value = JSON.parse(customerTmp);
+}
+function customerInsert() {
+    const new_customer = {
+        num: num.value,
+        name: name.value,
+        address: address.value
+    }
+    customers.value.push(new_customer);
+    localStorage.setItem("customerlist", JSON.stringify(customers.value));
+
+}
+</script>
+
+<template>
+    <div>
+        <h1> Vue Customer Management</h1>
+        킥킥,,,킥.,,,,,,,킥!!
+        <div class="container">
+            <form id="form1" class="form-horizontal">
+                <h2>Spring Boot</h2>
+                <div class="form-group">
+                    <label>번호:</label>
+                    <input type="text" class="form-control" id="num" v-model="num">
+                </div>
+                <div class="form-group">
+                    <label>이름:</label>
+                    <input type="text" class="form-control" id="name" v-model="name">
+                </div>
+
+                <div class="form-group">
+                    <label>주소:</label>
+                    <input type="text" class="form-control" id="address" v-model="address">
+                </div>
+
+                <div class="btn-group">
+                    <input type="button" class="btn btn-primary" value="등록" id="btnInsert" @click="customerInsert()" />
+                    <input type="button" class="btn btn-primary" value="수정" id="btnUpdate" @click="customerUpdate()" />
+                    <input type="button" class="btn btn-primary" value="삭제" id="btnDelete" @click="customerDelete()" />
+                    <input type="button" class="btn btn-primary" value="검색" id="btnSearch" @click="customerSearch()" />
+                    <input type="button" class="btn btn-primary" value="전체" id="btnAll" @click="selectAll()" />
+                    <input type="reset" class="btn btn-primary" value="초기화" id="btnInit" />
+
+                </div>
+            </form>
+        </div>
+        <hr />
+        <div class="container">
+            <h2>사용자 목록</h2>
+            <div v-if="customers.length > 0">
+                <table class="table text-center">
+                    <thead>
+                        <tr>
+                            <th class="text-center">번호</th>
+                            <th class="text-center">이름</th>
+                            <th class="text-center">주소</th>
+                        </tr>
+                    </thead>
+                    <tbody id="tb">
+                        <tr v-for="customer in customers" :key="customer.num">
+                            <td>{{ customer.num }}</td>
+                            <td>{{ customer.name }}</td>
+                            <td>{{ customer.address }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div v-else>
+                <h4>등록된 사용자가 없습니다.</h4>
+
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style scoped></style>
